6531d291b5cb3f8165f7684b02e54cbfc3cd8dcb
Diksha Chaudhary <97454110+dikshachaudhary25@users.noreply.github.com>
2025-01-03 21:24:02 +0530

    Create students_and_examinations.sql

---
+++ students_and_examinations.sql
@@ -0,0 +1,1 @@
+select s.student_id, s.student_name, s1.subject_name, count(e.student_id) as attended_exams from Students s cross join Subjects s1 left join Examinations e on s.student_id = e.student_id and s1.subject_name = e.subject_name  group by s1.subject_name , s.student_name, s.student_id order by s.student_id , s1.subject_name;
